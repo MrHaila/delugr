81c7f5961ce069eeb5b834d5bd08da02716b1a60
Teemu Haila <teemu.haila@metaplay.io>
2023-04-07 15:01:33 +0300

    Waveform preview of samples with audio playback

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,11 +10,10 @@ div(class="space-y-5 p-3 overflow-y-auto w-full")
       p Kits: {{ store.kits.length }}
       p Samples: {{ store.samples.length }}
 
-    p The Deluge has three types of entities: songs, kits and synths (internally called 'sounds'). Synths and kits can be saved as presets. Interestingly, songs and kits actually contain full instances (modified copies) of the original preset instead of just a reference to the used preset. This makes tracking usage statistics very complicated, so bear with me as I figure this out!
+    p The Deluge has three types of entities: songs, kits and synths (internally called 'sounds'). Synths and kits can be saved as presets. Songs and kits actually contain full instances (modified copies) of the original preset instead of just a reference to the used preset.
     
-    p(class="text-gray-400")
-      div There are sure to be obvious bugs and missing stuff. Feel free to report them in the repo and maybe even help out by leaving a PR!
-      div P.S. I don't have any firmware v1 or v2 songs to test with. Send me one if you have one!
+    p(class="text-gray-400") There are sure to be obvious bugs and missing stuff. Feel free to report them in the repo and maybe even help out by leaving a PR!
+    p(class="text-gray-400") P.S. I don't have any firmware v1 or v2 songs to test with. Send me one if you have one!
 
   div(class="flex space-x-3")
     //HCard(
